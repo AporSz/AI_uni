835e3fe1dcb96fd603e01bec2113356c00b98bc4
AporSz <szakacsapor2004@gmail.com>
2026-02-25 00:57:42 +0200

    new files

--- lab1/main.py
+++ lab1/main.py
@@ -5,8 +5,8 @@
 ##      nr of candidates
 
 import csv
-import random
-import numpy as np
+
+from lab1.randomArray import solve_with_random
 
 def get_nr_of_candidates():
     try:
@@ -54,40 +54,6 @@ def get_dataset(n):
                 break
 
         return problems
-
-def calculate_value(weights, prices, random_array, capacity):
-    current_capacity = 0
-    current_value = 0
-    for i in range(SIZE):
-        if random_array[i] == 1:
-            current_capacity += weights[i]
-            current_value += prices[i]
-    if current_capacity > capacity:
-        return 0
-    return current_value
-
-def solve_with_random(problem):
-    weights, prices, capacity = problem
-
-    best_solution = None
-    max_value = 0
-
-    i = 0
-    while i < NR_OF_PROBLEMS:
-        r = random.randint(0, np.pow(2, SIZE) - 1)
-        random_array = np.unpackbits(np.uint8(r))[3:]
-
-        value = calculate_value(weights, prices, random_array, capacity)
-        if value == 0:
-            i -= 1
-
-        if max_value < value:
-            max_value = value
-            best_solution = [random_array,weights,prices,max_value]
-
-        i += 1
-
-    return best_solution
 
 def solve_problems(problems):
     for problem in range(len(problems)):
