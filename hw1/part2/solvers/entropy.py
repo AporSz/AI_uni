ccacb13bdbb92a45736c9e69982232ccb2a8dafa
AporSz <szakacsapor2004@gmail.com>
2026-03-15 15:11:33 +0200

    idk what i'm doing

--- hw1/part2/solvers/entropy.py
+++ hw1/part2/solvers/entropy.py
@@ -4,8 +4,16 @@ class Entropy:
     def __init__(self, data):
         self._data = data
 
-    def calculate_entropy(self,) -> float:
-        pass
+    def calculate_entropy(self, attribute) -> float:
+        visited = set()
+        entropy = 0
+        for entry in self._data:
+            value = entry[attribute]
+            if value not in visited:
+                visited.add(value)
+                probability = self.calculate_probability(attribute, value)
+                entropy -= probability * np.log2(probability)
+        return entropy
 
     def calculate_probability(self, attribute, value) -> float:
         n = len(self._data)
@@ -15,3 +23,9 @@ class Entropy:
                 i += 1
 
         return i/n
+    
+if __name__ == "__main__":
+    data_loader = DataLoader('hw1/part2/data/PlayTennis.csv')
+    data = data_loader.get_data()
+    entropy_calculator = Entropy(data)
+    print(entropy_calculator.calculate_entropy('Play Tennis'))
